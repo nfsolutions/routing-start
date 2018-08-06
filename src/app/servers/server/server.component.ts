2ed527bd933e4110e2fc6292b83d8e73c2445879
nydi <stefan.nydegger@gmail.com>
2018-08-06 20:24:53 +0200

    lecture 135

--- src/app/servers/server/server.component.ts
+++ src/app/servers/server/server.component.ts
@@ -1,7 +1,8 @@
 import {Component, OnInit} from '@angular/core';
 
 import {ServersService} from '../servers.service';
-import {ActivatedRoute, Router} from '@angular/router';
+import {ActivatedRoute, Params, Router} from '@angular/router';
+import {Subscription} from 'rxjs';
 
 @Component({
   selector: 'app-server',
@@ -10,13 +11,22 @@ import {ActivatedRoute, Router} from '@angular/router';
 })
 export class ServerComponent implements OnInit {
   server: {id: number, name: string, status: string};
+  serverId = 0;
+  paramsSubscription: Subscription;
 
   constructor(private readonly serversService: ServersService,
               private readonly router: Router,
               private readonly route: ActivatedRoute) { }
 
   ngOnInit() {
-    this.server = this.serversService.getServer(1);
+    this.serverId = Number(this.route.snapshot.params['id']);
+    this.server = this.serversService.getServer(this.serverId);
+    this.paramsSubscription = this.route.params.subscribe(
+      (params: Params) => {
+        this.serverId = Number(params['id']);
+        this.server = this.serversService.getServer(this.serverId);
+      }
+    );
   }
 
   onEdit() {
