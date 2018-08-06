0b9b31ee102f8edd99efcd43b72e62e138790d92
nydi <stefan.nydegger@gmail.com>
2018-08-06 20:52:37 +0200

    lecture 136

---
+++ src/app/auth.service.ts
@@ -0,0 +1,24 @@
+import {reject} from 'q';
+
+export class AuthService {
+  loggedIn = false;
+
+  login() {
+    this.loggedIn = true;
+  }
+
+  logout() {
+    this.loggedIn = false;
+  }
+
+  isAuthenticated() {
+    const promise = new Promise(
+      (resolve, reject) => {
+        setTimeout(() => {
+          resolve(this.loggedIn);
+        }, 800);
+      }
+    );
+    return promise;
+  }
+}
